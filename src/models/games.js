dbbf9c9c4478c1183f4c351fcef788353be77d59
Romain Quilliot <romain.addweb@gmail.com>
2015-05-26 02:20:21 +0200

    Add Games model draft

---
+++ src/models/games.js
@@ -0,0 +1,77 @@
+var sha1 = require('sha1');
+var Games = function () {
+    this.games = {};
+};
+
+/**
+ * Generate a unique identifier
+ * @return string
+ */
+Games.prototype.generateID = function () {
+    do {
+        var identifier = sha1(Math.random() * 1000).substr(0, 7);
+    } while (this.existsGame(identifier));
+
+    return identifier;
+};
+
+/**
+ * Check if a game exists
+ * @param string id
+ * @return bool
+ */
+Games.prototype.existsGame = function (id) {
+    return (this.games[id]) ? true : false;
+};
+
+/**
+ * Add a game
+ * @return string
+ */
+Games.prototype.add = function () {
+    var identifier = this.generateID();
+    this.games[identifier] = {users: [], rank: 0};
+
+    return identifier;
+};
+
+/**
+ * Add a user to a game
+ * @param string game
+ * @param User userInstance
+ */
+Games.prototype.addUser = function (game, userInstance) {
+    this.games[game].users.push(userInstance);
+};
+
+/**
+ * Get games
+ * @param string id
+ * @return array|object
+ */
+Games.prototype.get = function (id) {
+    if (!id) return this.games;
+
+    return this.games[id];
+}
+
+/**
+ * Get users from game
+ * @param string id
+ * @return array
+ */
+Games.prototype.getUsers = function (id) {
+    return this.games[id].users;
+};
+
+/**
+ * Get users count from game
+ * @param string id
+ * @return int
+ */
+Games.prototype.countUsers = function (id) {
+    return this.games[id].users.length;
+};
+
+
+module.exports = Games;
