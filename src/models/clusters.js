53cbe0ef344d539d9d4ea6d00f9388364d4007cb
Romain Quilliot <romain.addweb@gmail.com>
2015-05-30 21:11:58 +0200

    Add clusters model

---
+++ src/models/clusters.js
@@ -0,0 +1,53 @@
+var Clusters = function () {
+    this.clusters = {};
+};
+
+
+/**
+ * Add a cluster
+ * @param string clusterName
+ */
+Clusters.prototype.add = function (clusterName) {
+    this.clusters[clusterName] = [];
+};
+
+
+/**
+ * Add a row in a cluster
+ * @param string clusterName
+ * @param mixed key
+ * @param mixed value
+ */
+Clusters.prototype.addTo = function (clusterName, key, value) {
+    if (!value) {
+        var value = key;
+        key = this.clusters.length;
+    }
+
+    this.clusters[clusterName][key] = value;
+
+    return this;
+};
+
+
+/**
+ * Get rows of a cluster
+ * @param string clusterName
+ * @return object
+ */
+Clusters.prototype.get = function (clusterName) {
+    return this.clusters[clusterName];
+};
+
+
+/**
+ * Get a row of a cluster
+ * @param string clusterName
+ * @return mixed
+ */
+Clusters.prototype.getFrom = function (clusterName, key) {
+    return this.get(clusterName)[key];
+};
+
+
+module.exports = Clusters;
