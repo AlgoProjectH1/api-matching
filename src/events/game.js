a61902b4d36ef0b47fd60c1602c392da744aa0be
Romain Quilliot <romain.addweb@gmail.com>
2015-06-17 15:04:22 +0200

    Add game events

--- src/events/game.js
+++ src/events/game.js
@@ -3,8 +3,23 @@ var Game = {};
 
 /**
  * When a user make a move on the game
+ * @param object move
  */
-Game.move = function (socket) {
+Game.move = function (socket, move) {
+
+};
+
+/**
+ * When a user give up the current game
+ */
+Game.giveUp = function (socket) {
+
+};
+
+/**
+ * When a user skip his turn
+ */
+Game.skip = function (socket) {
 
 };
 
