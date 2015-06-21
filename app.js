393d269af8552666273b58e6534ac4ab193cd5cf
Romain Quilliot <romain.addweb@gmail.com>
2015-06-21 18:24:31 +0200

    Enable disconnect events

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ io.on('connection', function (socket) {
 
     // When a player cancel
     socket.on('search:cancel', function () {
-        //global.events.search.leave(socket);
+        global.events.search.leave(socket);
     });
 
 
@@ -78,8 +78,7 @@ io.on('connection', function (socket) {
 
     // When a user leave a game
     socket.on('disconnect', function () {
-        //console.log('DISCONNECT EVENT');
-        //global.events.search.leave(socket);
+        global.events.search.leave(socket);
     });
 
 });
